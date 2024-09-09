4f560f49629e34d7cb25fae20ffcc97afe8478e8
wxia <19720270+lcxw@users.noreply.github.com>
2024-09-09 22:20:46 +0800

    init client template

---
+++ tunnel-client/src/main/java/com/lunar/cloud/tunnel/client/ProxyClientApplication.java
@@ -0,0 +1,28 @@
+package com.lunar.cloud.tunnel.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+@Slf4j
+@EnableScheduling // 开启定时任务功能
+@EnableAsync
+@EnableCaching
+public class ProxyClientApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyClientApplication.class, args);
+//        todo see git@github.com:3813g00c/flycat.git
+        log.info("> todo see git@github.com:3813g00c/flycat.git");
+        log.info("> todo see git@github.com:kdyzm/trojan-client-netty.git");
+        log.info("> todo see git@github.com:kongwu-/netty-proxy-server.git");
+    }
+
+}
